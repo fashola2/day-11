af1105a945e27ea68d77dcc0236f5f2702719f91
Ayodeji Fashola <fashola2@Ayodejis-MacBook-Pro.local>
2017-07-31 15:55:25 -0500

    complete

---
+++ main.js
@@ -0,0 +1,119 @@
+// The Form Data
+let formData = [
+  // For demonstration purposes,
+  // the first form element has been added to the HTML file as a comment
+  // compare the input in the HTML file with the contents of this first object
+  {
+    "type": "text",
+    "label": "First Name",
+    "id": "user-first-name",
+    "icon": "fa-user",
+    "options": []
+  },
+  {
+    "type": "text",
+    "label": "Last Name",
+    "id": "user-last-name",
+    "icon": "fa-user",
+    "options": []
+  },
+  {
+    "type": "email",
+    "label": "Email Address",
+    "id": "user-email",
+    "icon": "fa-envelope",
+    "options": []
+  },
+  {
+    "type": "text",
+    "label": "Current Website URL",
+    "id": "user-website",
+    "icon": "fa-globe",
+    "options": []
+  },
+  {
+    "type": "select",
+    "label": "Select Language",
+    "id": "user-language",
+    "icon": "",
+    "options": [
+      {
+        "label": "English",
+        "value": "EN"
+      },
+      {
+        "label": "French",
+        "value": "FR"
+      },
+      {
+        "label": "Spanish",
+        "value": "SP"
+      },
+      {
+        "label": "Chinese",
+        "value": "CH"
+      },
+      {
+        "label": "Japanese",
+        "value": "JP"
+      }
+    ]
+  },
+  {
+    "type": "textarea",
+    "label": "Your Comment",
+    "id": "user-comment",
+    "icon": "fa-comments",
+    "options": []
+  },
+  {
+    "type": "tel",
+    "label": "Mobile Number",
+    "id": "user-mobile",
+    "icon": "fa-mobile-phone",
+    "options": []
+  },
+  {
+    "type": "tel",
+    "label": "Home Number",
+    "id": "user-phone",
+    "icon": "fa-phone",
+    "options": []
+  }
+];
+
+// HINTS:
+// As you can see, we access the first element in the array
+// with [0] and then grab the property "label" using the "." operator
+
+
+
+// -------- Your Code Goes Below this Line --------
+
+let formDatalist = document.querySelector("#fields")
+
+for( let i = 0; i < formData.length; i++ ){
+
+let form = formData[i];
+
+
+if(form.type == "textarea") {
+  let textarea = document.createElement("textarea")
+  textarea.placeholder = form.label
+  formDatalist.append(textarea)
+} else if (form.type == "select") {
+    let select = document.createElement("select")
+    formDatalist.append(select)
+    for (var e = 0; e < form.options.length; e++) {
+      let option = document.createElement("option")
+       option.label = form.options[e].label;
+      select.add(option)
+    }
+  } else {
+    let textBox = document.createElement("input")
+    textBox.type = form.type;
+    textBox.placeholder = form.label;
+    formDatalist.append(textBox)
+  }
+}
+console.log(formDatalist)
